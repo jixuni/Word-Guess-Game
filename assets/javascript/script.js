f62d1006fd6e9166810e460b1cedde6ae02ee3d2
Jixu NI <40869621+jixuni@users.noreply.github.com>
2018-10-13 18:23:56 -0400

    added audio

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,5 @@
 
+// Delcare my variables
 var wrongGuess = [];
 
 var lifeLeft = 7;
@@ -23,6 +24,7 @@ const zodiac = [
     "dog"
 ];
 
+// Function for the number of _ base on lenght of word
 var randomNum = Math.floor(Math.random() * zodiac.length);
 
 var chooseWord = zodiac[randomNum];
@@ -39,7 +41,7 @@ randomWord();
 
 htmlMagicWord.textContent = magicWord.join(" "); // the .join method removes the , next to the _ 
 
-
+// Main function to compare key press to index of chosen word
 document.onkeyup = function(event){
     playerGuess = event.key
 
@@ -66,6 +68,8 @@ document.onkeyup = function(event){
 
 }
 //console.log(chooseWord); bug fixing
+
+// a counter to see if condition for win/lose is meet
 function winnerOrLoser() {
     if (winningCounter === chooseWord.length){
         alert("You win");
@@ -74,22 +78,27 @@ function winnerOrLoser() {
     }
 }
 
-
+// plays "bad" audio and appends the wrong guess to html
 function replaceWrong(){
     var htmlWrongGuess = document.getElementById("wrong_guess");
     htmlWrongGuess.textContent = wrongGuess;
+    document.getElementById("audio").play();
+    
 }
-
+// outputs the life/guesses left
 function remainingLife(){
     var guessLeft = document.getElementById("lifeLeft")
     guessLeft.textContent = lifeLeft;
 }
-
+// plays "good" audio and replace _ with correct letters
 function youGotIt(){
     var htmlCorrectGuess = document.getElementById("magic_word");
     htmlCorrectGuess.textContent = magicWord.join(" ");
+    document.getElementById("audio2").play();
 }
 
+// resets the game
 function reset(){
     window.location.reload();
 }
+
